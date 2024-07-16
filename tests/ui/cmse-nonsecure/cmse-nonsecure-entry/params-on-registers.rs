1d8e45897f948a93cf29fe9d6e055c46511df192
Folkert <folkert@folkertdev.nl>
2024-07-16 14:15:18 +0200

    remove CHECK comments; they don't appear to work

--- tests/ui/cmse-nonsecure/cmse-nonsecure-entry/params-on-registers.rs
+++ tests/ui/cmse-nonsecure/cmse-nonsecure-entry/params-on-registers.rs
@@ -1,7 +1,6 @@
+//@ build-pass
 //@ compile-flags: --target thumbv8m.main-none-eabi --crate-type lib
 //@ needs-llvm-components: arm
-//@ assembly-output: emit-asm
-//@ needs-asm-support
 #![feature(cmse_nonsecure_entry, no_core, lang_items)]
 #![no_core]
 #![crate_type = "lib"]
@@ -11,12 +10,7 @@ trait Sized {}
 trait Copy {}
 impl Copy for u32 {}
 
-//@ build-pass
-// @ compile-flags: -C no-prepopulate-passes -Zbranch-protection=bti
-
-// CHECK-LABEL: @entry_function
 #[no_mangle]
 pub extern "C-cmse-nonsecure-entry" fn entry_function(_: u32, _: u32, _: u32, d: u32) -> u32 {
-    // CHECK: florpsz
     d
 }
